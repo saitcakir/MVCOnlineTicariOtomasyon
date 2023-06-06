a6dd4251af436fbfc89bee74d22866ab16f5c266
Sait ÇAKIR <sait25@windowslive.com>
2023-06-06 13:21:14 +0300

    Add project files.

---
+++ MVCOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int Cariid { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(13)]
+        public string CariSehir { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+
+    }
+}
---
+++ MVCOnlineTicariOtomasyon/Models/Siniflar/Departman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Departman
+    {
+        [Key]
+        public int Departmanİd { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string DepartmanAd { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+    }
+}
